1132d4b94a14233a2d55d007e34b0423e0b19b6a
Glenn Hope <glenn.alexander.hope@gmail.com>
2025-06-02 13:00:15 +0200

    Initial script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# NOTE: things added here MUST be idempotent
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+mkdir -p /srv/map_cache/maps
+
+for mapdir in /root/tf2-*/tf/maps/; do
+    rsync -a "$mapdir"/ /srv/map_cache/maps/
+done
+
