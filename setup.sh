9a16f3a0f0383a7ae86b442c96b15cf1d1d34145
Glenn Hope <glenn.alexander.hope@gmail.com>
2025-09-12 09:52:05 +0200

    Add comment for future self

--- setup.sh
+++ setup.sh
@@ -22,6 +22,7 @@ pushd systemd
 
 systemctl enable --now tf2-fastdl.service
 for timer in *.timer; do
+    # NOTE: you may need to --now or start these as well, but at least once that caused an instant server restart so double-check that in the future and verify with list-timers that these are actually running
 	systemctl enable "$timer"
 done
 
