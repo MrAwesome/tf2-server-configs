65d9c7096352eb1a1139f3d35b0e7e94ce8c23e2
Glenn Hope <glenn.alexander.hope@gmail.com>
2025-11-01 22:11:08 +0200

    Update script to always wipe steamapps

--- bin/restart_server.sh
+++ bin/restart_server.sh
@@ -6,10 +6,14 @@ servername="${1:-}"
 
 cd /root/docker
 if [[ -z "$servername" ]]; then
+    echo "[WARN] Warning! Restarting all servers at once, this is not recommended."
+    sleep 4
 	docker-compose down
 	/root/bin/run_server.sh 
 else
 	docker stop "$servername"
 	docker remove "$servername"
+    steamapps="${HOME}/servers/tf2-${servername}/steamapps/"
+    [[ -d "$steamapps" ]] && rm -rf "$steamapps"
 	/root/bin/run_server.sh "$servername"
 fi
